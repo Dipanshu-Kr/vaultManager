8208fd388418cf7e4fc109a0e591a0aecfc60fd0
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-04-29 05:28:35 +0530

    Update database password in db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Dipanshu@4512",
+  password: "PASSWORD",
   database: "password_vault",
 });
 
